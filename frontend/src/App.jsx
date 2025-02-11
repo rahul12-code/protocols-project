681e1a19121d058b24a8c4d6d58936393f2ac019
rahul <rahulkandati@gmail.com>
2025-02-11 15:24:26 +0530

    Added frontend and removed unnecessary Git tracking from backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className='bg-red-500 italic text-5xl'>Hello React</h1>
+      
+    </>
+  )
+}
+
+export default App
