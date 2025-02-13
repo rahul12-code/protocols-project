2f12297e5849514419628759e3f5316f8bfa99bc
rahul <rahulkandati@gmail.com>
2025-02-13 17:47:36 +0530

    Updated PageNotFound page using Material-UI

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -1,5 +1,27 @@
+import { Box, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      textAlign="center"
+    >
+      <Typography variant="h1" color="error">
+        404
+      </Typography>
+      <Typography variant="h5" marginBottom={2}>
+        Sorry, the page you visited does not exist.
+      </Typography>
+      <Button variant="contained" color="primary" component={Link} to="/">
+        Back to Home
+      </Button>
+    </Box>
+  );
 };
 
 export default PageNotFound;
